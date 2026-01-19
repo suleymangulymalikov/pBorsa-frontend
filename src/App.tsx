9d91e89a0632da41b4decb4d2c3c47fc6d351b90
Suleyman <suleymanmalikov03@gmail.com>
2026-01-19 15:22:58 +0100

    chore: created react+vite and setup tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600 underline">
+        Tailwind is working ✅
+      </h1>
+    </div>
+  );
+}
